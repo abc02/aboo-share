680ecce68899ca65e91968390a71f890a3ea38e4
abc02 <abcgg02@gmail.com>
2018-03-23 14:45:20 +0800

    update package name

--- src/main.js
+++ src/main.js
@@ -4,22 +4,22 @@ import 'styles/reset.scss'
 import 'styles/index.scss'
 import 'styles/wxtip.scss'
 import 'animate.css'
-window.onload = function () {
-    console.log('load')
-    function weixinTip(ele) {
-        var ua = navigator.userAgent;
-        var isWeixin = !!/MicroMessenger/i.test(ua);
-        if (isWeixin) {
-            ele.onclick = function (e) {
-                window.event ? window.event.returnValue = false : e.preventDefault();
-                document.getElementById('JweixinTip').style.display = 'block';
-            }
-            document.getElementById('JweixinTip').onclick = function () {
-                this.style.display = 'none';
-            }
-        }
-    }
-    var btn1 = document.getElementById('button-donwload');//下载一
-    weixinTip(btn1);
-}
+// window.onload = function () {
+//     console.log('load')
+//     function weixinTip(ele) {
+//         var ua = navigator.userAgent;
+//         var isWeixin = !!/MicroMessenger/i.test(ua);
+//         if (isWeixin) {
+//             ele.onclick = function (e) {
+//                 window.event ? window.event.returnValue = false : e.preventDefault();
+//                 document.getElementById('JweixinTip').style.display = 'block';
+//             }
+//             document.getElementById('JweixinTip').onclick = function () {
+//                 this.style.display = 'none';
+//             }
+//         }
+//     }
+//     var btn1 = document.getElementById('button-donwload');//下载一
+//     weixinTip(btn1);
+// }
 
